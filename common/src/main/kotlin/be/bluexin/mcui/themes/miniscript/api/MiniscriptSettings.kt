ba0a703620cb4026de4b66cfb96235bf81d483a6
Arnaud 'Bluexin' Solé <bluexin.games@gmail.com>
2025-01-05 18:45:38 +0100

    Support references to settings from a specific theme (rather than the HUD theme)

--- common/src/main/kotlin/be/bluexin/mcui/themes/miniscript/api/MiniscriptSettings.kt
+++ common/src/main/kotlin/be/bluexin/mcui/themes/miniscript/api/MiniscriptSettings.kt
@@ -10,29 +10,54 @@ import org.koin.core.annotation.Single
 @Suppress("unused", "MemberVisibilityCanBePrivate") // Exposed to JEL
 interface MiniscriptSettings {
     /**
-     * @return a string setting value
+     * @return a string setting value with given [key]
      */
     fun string(key: String): String
 
     /**
-     * @return a boolean setting value
+     * @return a boolean setting value with given [key]
      */
     fun boolean(key: String): Boolean
 
     /**
-     * @return an integer setting value
+     * @return an integer setting value with given [key]
      */
     fun int(key: String): Int
 
     /**
-     * @return a decimal number setting value
+     * @return a decimal number setting value with given [key]
      */
     fun double(key: String): Double
 
     /**
-     * @return a resource location setting value
+     * @return a resource location setting value with given [key]
      */
     fun resourceLocation(key: String): ResourceLocation
+
+    /**
+     * @return a string setting value from [themeId] with given [key]
+     */
+    fun string(themeId: String, key: String): String
+
+    /**
+     * @return a boolean setting value from [themeId] with given [key]
+     */
+    fun boolean(themeId: String, key: String): Boolean
+
+    /**
+     * @return an integer setting value from [themeId] with given [key]
+     */
+    fun int(themeId: String, key: String): Int
+
+    /**
+     * @return a decimal number setting value from [themeId] with given [key]
+     */
+    fun double(themeId: String, key: String): Double
+
+    /**
+     * @return a resource location setting value from [themeId] with given [key]
+     */
+    fun resourceLocation(themeId: String, key: String): ResourceLocation
 }
 
 @Single
@@ -49,4 +74,14 @@ internal class MiniscriptSettingsImpl(
     override fun double(key: String) = setting(key) as? Double ?: .0
     override fun resourceLocation(key: String) =
         setting(key) as? ResourceLocation ?: ResourceLocation(Constants.MOD_ID, "missing")
+
+    private fun setting(themeId: String, key: String): Any? =
+        Settings[ResourceLocation(themeId), ResourceLocation(key)]
+
+    override fun string(themeId: String, key: String) = (setting(themeId, key) as? String).orEmpty()
+    override fun boolean(themeId: String, key: String) = setting(themeId, key) as? Boolean ?: false
+    override fun int(themeId: String, key: String) = setting(themeId, key) as? Int ?: 0
+    override fun double(themeId: String, key: String) = setting(themeId, key) as? Double ?: .0
+    override fun resourceLocation(themeId: String, key: String) =
+        setting(themeId, key) as? ResourceLocation ?: ResourceLocation(Constants.MOD_ID, "missing")
 }
